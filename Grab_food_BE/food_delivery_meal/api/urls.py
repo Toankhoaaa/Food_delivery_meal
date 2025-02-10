b729f00966498deda9826142534dde04ee33503c
TuanVu219 <supersaza219@gmail.com>
2025-02-10 13:40:03 +0700

    Login

--- Grab_food_BE/food_delivery_meal/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from .views import *
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('api/login', LoginView.as_view(), name='login'),
-    path('api/register', CustomerRegistrationView.as_view(), name='register'),
-]
